e09b3d32692038a5b94326e9dd03fef6e50d5841
Gianni <gianniicherry@gmail.com>
2022-09-23 09:21:13 -0700

    commit tree 1d99909656eefa7a3cec3b525cedb15338d175a2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const birthday = "birthday"
+function writeCards(names,birthday){
+    const messages = []
+   for(let i=0; i < names.length; i++){
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+    }
+    return messages
+}
+function countDown(){
+    let countDown = 10
+    while(countDown >= 0)
+    console.log(countDown--)
+}
